c2af1bafd15085420fd4e4037fbf05d389aac010
Lacosta <info@projectideal.nl>
2013-07-04 17:08:34 +0200

    Update library.js

--- library.js
+++ library.js
@@ -1,13 +1,23 @@
+/* By Dominic Vonk (based on jQuery) */
 (function (window, undefined) {
-    var
-    _$ = window.$,
+    var _$_ = window.$_ || window.libraries || [],
+        _$ = window.$,
         _Mimbu = window.Mimbu,
-        version = "0.0.1a",
-        library = "Mimbu";
+        version = "0.0.2a",
+        library = "Mimbu",
+        _addtolist = true;
     Mimbu = function (selector, context) {
         return new Mimbu.fn.init(selector, context);
     };
-
+    var libraries = _$_.slice();
+   for(i = 0; i < libraries.length; i++) {
+    if(libraries[i] === library + " " + version) {
+      _addtolist = false;
+    }
+  }
+  if (_addtolist) {
+    libraries.push(library + " " + version);
+  }
     Mimbu.fn = Mimbu.prototype = {
         constructor: Mimbu,
         init: function (selector, context) {
@@ -20,7 +30,6 @@
             }
             for (var key in this) {
                 selector[key] = this[key];
-
             }
 
             return selector;
@@ -62,12 +71,17 @@
                 window.Mimbu = _Mimbu;
             }
             return Mimbu;
+        },
+        noListing: function () {
+            window.$_ = window.libraries = _$_;
         }
     });
+
     if (typeof module === "object" && module && typeof module.exports === "object") {
         module.exports = Mimbu;
     } else {
         window.Mimbu = window.$ = Mimbu;
+        window.$_ = window.libraries = libraries;
         if (typeof define === "function" && define.amd) {
             define("mimbu", [], function () {
                 return Mimbu;
@@ -77,7 +91,10 @@
 
 })(window);
 
-$.say = function (msg) {
-    console.log(msg);
-};
-$.say("ey");
+$.extend({
+    say: function (msg) {
+        console.log(msg);
+    }
+});
+$.say($.library + " " + $.version);
+$.say(libraries);
